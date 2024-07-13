25a70447c4dc9bd459d8b7d43c4bb83841591a9b
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2024-07-13 13:11:08 -0300

    **fix(template): clarify message length requirement in template**

--- src/template.ts
+++ src/template.ts
@@ -5,7 +5,7 @@ export const defaultPromptTemplate = [
   'commit messages should:',
   ' - follow conventional commits',
   ' - message format should be: <type>[scope]: <description>',
-  ' - minimum length of message is 90 characters',
+  ' - minimum length of message is 90 characters all in one line',
   '',
   'examples:',
   ' - fix(authentication): add password regex pattern',
